bfe53065a5eadf6e89e96817c4053b07b807970a
felip <felipe.saracho@lilab.pe>
2024-08-15 21:25:34 -0300

    Api response models completed

--- src/domain/model/club.ts
+++ src/domain/model/club.ts
@@ -1,4 +1,55 @@
+import { Slot } from './slot';
+
+export interface ClubLocation {
+  name: string;
+  city: string;
+  lat: string;
+  lng: string;
+}
+
+export interface ClubCountry {
+  id: number;
+  name: string;
+  iso_code: string;
+}
+
+export interface ClubZone {
+  id: number;
+  name: string;
+  full_name: string;
+  placeid: string;
+  country: ClubCountry;
+}
+
+export interface ClubProps {
+  sponsor: boolean;
+  favorite: boolean;
+  stars: string;
+  payment: boolean;
+}
+
+export type ClubAttributes = string[];
+
+export interface ClubOpenHour {
+  day_of_week: number;
+  open_time: number;
+  close_time: number;
+  open: boolean;
+}
+
 export interface Club {
   id: number;
-  // TODO: fill
+  permalink: string;
+  name: string;
+  logo: string;
+  logo_url: string;
+  background: string;
+  background_url: string;
+  location: Location;
+  zone: ClubZone;
+  props: ClubProps;
+  attributes: ClubAttributes;
+  openHours: ClubOpenHour[];
+  _priority: number;
+  available: Slot[];
 }
--- src/domain/model/court.ts
+++ src/domain/model/court.ts
@@ -1,4 +1,28 @@
+export interface CourtAttributes {
+  floor: string;
+  light: boolean;
+  roofed: boolean;
+  beelup: boolean;
+}
+
+export interface CourtSport {
+  id: number;
+  parent_id: number;
+  name: string;
+  players_max: number;
+  order: number;
+  default_duration: number;
+  divisible_duration: number;
+  icon: string;
+  pivot: {
+    court_id: number;
+    sport_id: number;
+    enabled: boolean;
+  };
+}
+
 export interface Court {
   id: number;
-  // TODO: fill
+  name: string;
+  sports: CourtSport[];
 }
