28ecd05d7436cba65cc6e3db56ce97471c81c544
Felipe Saracho <FelipeSaracho02@gmail.com>
2024-08-23 06:05:28 -0300

    Added redis and client+cache services in module

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -4,6 +4,7 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
+import * as redisStore from 'cache-manager-ioredis';
 
 import { ClubUpdatedHandler } from '../domain/handlers/events/club-updated.handler';
 import { CourtUpdatedHandler } from '../domain/handlers/events/court-updated.handler';
@@ -11,27 +12,40 @@ import { SlotBookedHandler } from '../domain/handlers/events/slot-booked.handler
 import { SlotCanceledHandler } from '../domain/handlers/events/slot-canceled.handler';
 import { GetAvailabilityHandler } from '../domain/handlers/get-availability.handler';
 import { GetZonesHandler } from '../domain/handlers/get-zones.handler';
-import { ALQUILA_TU_CANCHA_CLIENT } from '../domain/ports/aquila-tu-cancha.client';
 import { HTTPAlquilaTuCanchaClient } from '../infrastructure/clients/http-alquila-tu-cancha.client';
+import { ATC_CLIENT_QUEUE } from '../infrastructure/constants/queue';
 import { EventsController } from '../infrastructure/controllers/events.controller';
 import { SearchController } from '../infrastructure/controllers/search.controller';
 import { ZonesController } from '../infrastructure/controllers/zones.controller';
+import { ALQUILA_TU_CANCHA_CLIENT } from '../infrastructure/interfaces/aquila-tu-cancha.client';
+import { ApiRequestProcessor } from '../infrastructure/processors/atc-client-queue.processor';
+import { ClubsService } from './services/atc-client-service/clubs.service';
+import { CourtsService } from './services/atc-client-service/courts.service';
+import { SlotsService } from './services/atc-client-service/slots.service';
+import { ZonesService } from './services/atc-client-service/zones.service';
 
 @Module({
   imports: [
     HttpModule,
     CqrsModule,
     ConfigModule.forRoot(),
-    CacheModule.register({ isGlobal: true }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6380,
+    }),
     BullModule.forRootAsync({
       useFactory: () => ({
         redis: {
           host: 'localhost',
-          port: 6379,
+          port: 6380,
         },
       }),
     }),
-    BullModule.registerQueueAsync({ name: 'apiRequests' }),
+    BullModule.registerQueueAsync({
+      name: ATC_CLIENT_QUEUE,
+    }),
   ],
   controllers: [SearchController, EventsController, ZonesController],
   providers: [
@@ -45,6 +59,11 @@ import { ZonesController } from '../infrastructure/controllers/zones.controller'
     SlotBookedHandler,
     SlotCanceledHandler,
     GetZonesHandler,
+    ApiRequestProcessor,
+    ZonesService,
+    ClubsService,
+    SlotsService,
+    CourtsService,
   ],
 })
 export class AppModule {}
